d47a2a81ac7e38f7aca4d297c5901af2b090c01a
shrub <shrub719@gmail.com>
2025-09-08 11:37:23 +0100

    boilerplate tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include "eadkpp.h"
 #include <eadk.h>
+// eadk.h needed for eadk_display_wait_for_vblank
+// TODO: eadkpp might have a hidden wait function, double check
 using namespace EADK;
 
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "3D Grapher";
