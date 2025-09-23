8feb44d2de27692de3808bd1f56b937c59cbe10b
shrub <shrub719@gmail.com>
2025-09-23 22:46:42 +0100

    used Vec::with_capacity

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ pub mod settings {
 }
 
 pub mod test {
-    pub const TEST_N: usize = 8;
+    pub const TEST_N: usize = 5;
     pub const TEST_SCALE: f32 = 2.0;
 }
 
@@ -14,7 +14,8 @@ pub mod graphics {
     pub const MARGIN_BOTTOM: usize = 36;
     pub const MARGIN_SIDE: usize = 0;
 
-    pub const FB_TILE: usize = 2;
+    // 2 is possible with margins and very few triangles... i kinda wanna try stretch it further
+    pub const FB_TILE: usize = 3;
 
     pub const FB_WIDTH: usize = (SCREEN_WIDTH - 2 * MARGIN_SIDE) / FB_TILE;
     pub const FB_HEIGHT: usize = (SCREEN_HEIGHT - (MARGIN_TOP + MARGIN_BOTTOM)) / FB_TILE;
@@ -22,4 +23,6 @@ pub mod graphics {
 
 pub mod limits {
     pub const MAX_TRIS: usize = 500;
+    pub const MAX_POINTS: usize = 500;
+    pub const MAX_LINES: usize = 20;
 }
--- src/grapher.rs
+++ src/grapher.rs
@@ -1,8 +1,12 @@
 use crate::{
     renderer::*,
-    mesh::*,
-    input::*
+    mesh::{ *, Triangle },
+    mat::Vector3,
+    input::*,
+    config::*
 };
+#[cfg(target_os = "none")]
+use alloc::vec;
 
 pub struct Grapher {
     renderer: Renderer,
@@ -19,6 +23,34 @@ impl Grapher {
     }
 
     pub fn main_loop(&mut self) {
+        self.mesh.indices = vec![
+            Vector3::new(-3.0, -3.0, 10.0),
+            Vector3::new(-3.0, 3.0, 10.0),
+            Vector3::new(3.0, -3.0, 10.0),
+            Vector3::new(3.0, 3.0, 10.0),
+            Vector3::new(-3.0, -3.0, -10.0),
+            Vector3::new(-3.0, 3.0, -10.0),
+            Vector3::new(3.0, -3.0, -10.0),
+            Vector3::new(3.0, 3.0, -10.0),
+        ];
+        self.mesh.tris = [
+            // square1
+            [Triangle ([0, 1, 3]); test::TEST_N], 
+            [Triangle ([0, 2, 3]); test::TEST_N],
+
+            // square2
+            [Triangle ([4, 5, 7]); test::TEST_N], 
+            [Triangle ([4, 6, 7]); test::TEST_N],
+
+            // side1
+            [Triangle ([0, 1, 4]); test::TEST_N], 
+            [Triangle ([1, 4, 5]); test::TEST_N],
+
+            // side2
+            [Triangle ([0, 2, 4]); test::TEST_N], 
+            [Triangle ([2, 4, 6]); test::TEST_N],
+        ].concat().to_vec();
+
         // main loop - runs every frame
         while !self.input.upd.quit {
             if self.input.upd.domain {
