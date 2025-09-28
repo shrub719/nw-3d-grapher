313dd05269669ebf0d9485b4df504b3f2a98c7f8
shrub <shrub719@gmail.com>
2025-09-28 18:45:07 +0100

    added angle-to-quaternion

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,3 +1,4 @@
+use crate::trig::*;
 use crate::mat::*;
 use crate::eadk::info;
 use crate::eadk::random;
@@ -121,10 +122,21 @@ impl Mesh {
     pub fn update_rotation(&mut self, rotation_direction: Vector3, delta_time: f32) {
         // TODO: update for quaternions
         let rotation_speed = settings::ROTATION_SPEED * delta_time;
-        let x = rotation_direction.x * rotation_speed;
-        let y = rotation_direction.y * rotation_speed;
-        let z = rotation_direction.z * rotation_speed;
-        let q = Quaternion::new(1.0, x, y, z);
+        let x = rotation_direction.x * rotation_speed / 2.0;
+        let y = rotation_direction.y * rotation_speed / 2.0;
+        let z = rotation_direction.z * rotation_speed / 2.0;
+
+        let (cx, sx) = (cos(x), sin(x));
+        let (cy, sy) = (cos(y), sin(y));
+        let (cz, sz) = (cos(z), sin(z));
+
+        let q = Quaternion::new(
+            cx*cy*cz + sx*sy*sz, 
+            sx*cy*cz + cx*sy*sz, 
+            cx*sy*cz + sx*cy*sz, 
+            cx*cy*sz + sx*sy*cz
+        );
+        self.rotation *= q;
     }
 
     pub fn update_scale(&mut self, scale_change: f32, delta_time: f32) {
@@ -134,6 +146,7 @@ impl Mesh {
 
     pub fn transform(&mut self) {
         let mut matrix = PROJECTION_MATRIX;
+        // TODO: can you use quaternions to do the rotation without the translation?
         matrix *= self.rotation.get_rotation_matrix();
         matrix *= self.domain.matrix;
         matrix *= get_scale_matrix(self.scale);
