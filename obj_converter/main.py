eb7bf7436f5b411a53fabea9c8b68b37dd36c277
shrub <shrub719@gmail.com>
2025-10-04 23:51:27 +0100

    cleaned up obj converter script

--- obj_converter/main.py
+++ obj_converter/main.py
@@ -1,64 +1,53 @@
-# EXTREMELY sloppy way to convert obj file into rust code
+import struct, sys
 
-import struct
+def v_to_vertex(line):
+    vertex = line[2:].split()
+    return tuple(float(coord) for coord in vertex)
 
-vertices = []
-indices = []
+def f_to_face(line):
+    face = line[2:].split()
+    indices = [index.split("/")[0] for index in face]
 
-with open("mesh.obj", "r") as o:
-    lines = o.readlines()
-    for line in lines:
-        if line[0:2] == "v ":
-            print(line)
-            line = line[2:]
-            v = line.split()
-            vertices.append((
-                float(v[0]),
-                float(v[1]),
-                float(v[2])
-            ))
-        elif line[0] == "f":
-            line = line[2:]
-            face = line.split()
-            f = [vertex.split("/")[0] for vertex in face]
-            if len(f) == 3:
-                indices.append((
-                    int(f[0]),
-                    int(f[1]),
-                    int(f[2])
-                ))
-            elif len(f) == 4:
-                indices.append((
-                    int(f[0]),
-                    int(f[1]),
-                    int(f[2])
-                ))
-                indices.append((
-                    int(f[0]),
-                    int(f[2]),
-                    int(f[3])
-                ))
+    if len(indices) == 3:
+        return [tuple(int(index) for index in indices)]
+    
+    return [
+        tuple(int(indices[i]) for i in [0, 1, 2]),
+        tuple(int(indices[i]) for i in [0, 2, 3])
+    ]
 
-print("read tris")
+def extract_obj(filename):
+    vertices, faces = [], []
 
-tris = []
-for face in indices:
-    print(face)
-    v0 = vertices[face[0] - 1]
-    v1 = vertices[face[1] - 1]
-    v2 = vertices[face[2] - 1]
-    tri = [v0, v1, v2]
-    print(tri)
-    tris.append(tri)
+    with open(filename, "r") as o:
+        lines = o.readlines()
+        for line in lines:
+            if line[0:2] == "v ":
+                vertices.append(v_to_vertex(line))
+            elif line[0] == "f":
+                faces.extend(f_to_face(line))
 
-print("assembled tris")
+    print("extracted obj file")
+    return vertices, faces
 
-with open("mesh.pbj", "wb") as f:
-    for tri in tris:
-        # Flatten the triangle’s coordinates
-        flat = [coord for vertex in tri for coord in vertex]
-        # Pack as 9 floats, little endian
-        f.write(struct.pack("<9f", *flat))
+def obj_to_tris(vertices, faces):
+    tris = []
+    for face in faces:
+        tri = [vertices[index - 1] for index in face]
+        tris.append(tri)
+    
+    print("converted object to tris")
+    return tris
 
-print("done")
-print(len(tris))
+def pack_tris(tris):
+    with open("mesh.pbj", "wb") as f:
+        for tri in tris:
+            flat = [coord for vertex in tri for coord in vertex]
+            f.write(struct.pack("<9f", *flat))
+
+    print("packed tris to mesh.pbj")
+
+vertices, faces = extract_obj(sys.argv[1])
+tris = obj_to_tris(vertices, faces)
+pack_tris(tris)
+print(f"{len(tris)} tris")
