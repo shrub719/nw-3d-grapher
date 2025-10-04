6e8e32a12c7b1e3a7cccd9334d70ab23b88b17d2
shrub <shrub719@gmail.com>
2025-10-04 20:04:12 +0100

    adjusted hud

--- src/hud.rs
+++ src/hud.rs
@@ -15,8 +15,8 @@ fn draw_hud_string(text: &str, bg_color: Color) {
 pub fn draw_hud(mode: Mode, mode_update: bool, scale: f32, n: usize) {
     let bg_color = match mode {
         Mode::Rotate => RED,
-        Mode::Translate => BLUE,
-        Mode::Green => GREEN
+        Mode::Green => GREEN,
+        Mode::Translate => BLUE
     };
     
     if mode_update {
@@ -37,6 +37,20 @@ pub fn draw_hud(mode: Mode, mode_update: bool, scale: f32, n: usize) {
             Mode::Translate => format!("tris: {}      ", n),
             Mode::Green => format!("green")
         }),
+        bg_color
+    );
+    
+    let text = match mode {
+        Mode::Rotate => "VIEW",
+        Mode::Green => "TRACE",
+        Mode::Translate => "DOMAIN"
+    };
+    let length = text.len() as u16;
+    display::draw_string(
+        text,
+        Point { x: SCREEN_WIDTH as u16 - 8 * length, y: SCREEN_HEIGHT as u16 - 15},
+        false,
+        WHITE,
         bg_color
     );
 }
--- src/input.rs
+++ src/input.rs
@@ -14,8 +14,8 @@ fn bind_keys(keyboard_state: &KeyboardState, pos_key: Key, neg_key: Key, update:
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub enum Mode {
     Rotate,
-    Translate,
-    Green
+    Green,
+    Translate
 }
 
 #[derive(Default)]
@@ -78,14 +78,14 @@ impl InputHandler {
         }
         
         if self.keyboard_state.key_down(Key::Seven) { 
+            self.upd.mode = self.mode != Mode::Rotate;
             self.mode = Mode::Rotate; 
-            self.upd.mode = true;
         } else if self.keyboard_state.key_down(Key::Eight) { 
-            self.mode = Mode::Translate;
-            self.upd.mode = true;
-        } else if self.keyboard_state.key_down(Key::Nine) { 
+            self.upd.mode = self.mode != Mode::Green;
             self.mode = Mode::Green;
-            self.upd.mode = true;
+        } else if self.keyboard_state.key_down(Key::Nine) { 
+            self.upd.mode = self.mode != Mode::Translate;
+            self.mode = Mode::Translate;
         }
 
         if self.keyboard_state.key_down(Key::Home) {
