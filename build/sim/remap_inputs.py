5f8b5553244abbde0bacaae962f579c3e09397ec
shrub <shrub719@gmail.com>
2025-10-24 12:59:32 +0100

    use regex in remap_inputs to dynamically map from constants.rs

--- build/sim/remap_inputs.py
+++ build/sim/remap_inputs.py
@@ -1,67 +1,109 @@
 import sys, re
 
+REMAP_EXP = r'constexpr static KeySDLKeyPair sKeyPairs\[] ?= ?\{[\S\s]*?};'
+IDENTIFIER_EXP = r"pub const (\w+): Key ="
+KEY_EXP = r": Key =\s+(Key::\w+);"
+
+inputs = [
+    ["D_DOWN", "DOWN"],
+    ["D_DOWN", "S"],
+    ["D_UP", "UP"],
+    ["D_UP", "W"],
+    ["D_LEFT", "LEFT"],
+    ["D_LEFT", "A"],
+    ["D_RIGHT", "RIGHT"],
+    ["D_RIGHT", "D"],
+    ["D_SP_1", "LCTRL"],
+    ["D_SP_1", "RCTRL"],
+    ["D_SP_1", "E"],
+    ["D_SP_2", "LSHIFT"],
+    ["D_SP_2", "RSHIFT"],
+    ["D_SP_2", "Q"],
+
+    ["INCREASE", "KP_PLUS"],
+    ["INCREASE", "EQUALS"],
+    ["INCREASE", "R"],
+    ["DECREASE", "KP_MINUS"],
+    ["DECREASE", "MINUS"],
+    ["DECREASE", "F"],
+    ["MODIFIER", "0"],
+    ["DECREASE", "X"],
+
+    ["CONFIRM", "RETURN"],
+    ["BACK", "ESCAPE"],
+
+    ["HELP", "TAB"],
+    ["RESET", "BACKSPACE"],
+
+    ["MODE_1", "7"],
+    ["MODE_1B", "1"],
+    ["MODE_2", "8"],
+    ["MODE_2B", "2"],
+    ["MODE_3", "9"],
+    ["MODE_3B", "3"]
+]
+
+
 def info(string):
     print("    " + string)
 
 
-INPUTS = """
-constexpr static KeySDLKeyPair sKeyPairs[] = {
-  KeySDLKeyPair(Key::Down,      SDL_SCANCODE_DOWN),
-  KeySDLKeyPair(Key::Down,      SDL_SCANCODE_S),
-  KeySDLKeyPair(Key::Up,        SDL_SCANCODE_UP),
-  KeySDLKeyPair(Key::Up,        SDL_SCANCODE_W),
-  KeySDLKeyPair(Key::Left,      SDL_SCANCODE_LEFT),
-  KeySDLKeyPair(Key::Left,      SDL_SCANCODE_A),
-  KeySDLKeyPair(Key::Right,     SDL_SCANCODE_RIGHT),
-  KeySDLKeyPair(Key::Right,     SDL_SCANCODE_D),
+def remap_key_pair(identifier, key, inputs):
+    for i in range(len(inputs)):
+        if inputs[i][0] == identifier:
+            inputs[i][0] = key
 
-  KeySDLKeyPair(Key::Shift,     SDL_SCANCODE_LSHIFT),
-  KeySDLKeyPair(Key::Shift,     SDL_SCANCODE_RSHIFT),
-  KeySDLKeyPair(Key::Shift,     SDL_SCANCODE_Q),
-  KeySDLKeyPair(Key::Alpha,     SDL_SCANCODE_LCTRL),
-  KeySDLKeyPair(Key::Alpha,     SDL_SCANCODE_RCTRL),
-  KeySDLKeyPair(Key::Alpha,     SDL_SCANCODE_E),
 
-  KeySDLKeyPair(Key::Plus,      SDL_SCANCODE_KP_PLUS),
-  KeySDLKeyPair(Key::Plus,      SDL_SCANCODE_EQUALS),
-  KeySDLKeyPair(Key::Plus,      SDL_SCANCODE_R),
-  KeySDLKeyPair(Key::Minus,     SDL_SCANCODE_KP_MINUS),
-  KeySDLKeyPair(Key::Minus,     SDL_SCANCODE_MINUS),
-  KeySDLKeyPair(Key::Minus,     SDL_SCANCODE_F),
-  KeySDLKeyPair(Key::Multiplication, SDL_SCANCODE_0),
-  KeySDLKeyPair(Key::Multiplication, SDL_SCANCODE_X),
+def remap_controls(app_controls_file, inputs):
+    controls = ""
 
-  KeySDLKeyPair(Key::Ok,        SDL_SCANCODE_RETURN),
-  KeySDLKeyPair(Key::Back,      SDL_SCANCODE_ESCAPE),
+    with open(app_controls_file, "r") as f:
+        controls = f.read()
+    
+    for control in controls.split("\n"):
+        identifier_match = re.findall(IDENTIFIER_EXP, control)
+        key_match = re.findall(KEY_EXP, control)
+        if identifier_match and key_match:
+            remap_key_pair(identifier_match[0], key_match[0], inputs)
 
-  KeySDLKeyPair(Key::Toolbox,   SDL_SCANCODE_TAB),
-  KeySDLKeyPair(Key::Backspace, SDL_SCANCODE_BACKSPACE),
+    return inputs
 
-  KeySDLKeyPair(Key::Seven,     SDL_SCANCODE_7),
-  KeySDLKeyPair(Key::One,     SDL_SCANCODE_1),
-  KeySDLKeyPair(Key::Eight,     SDL_SCANCODE_8),
-  KeySDLKeyPair(Key::Two,     SDL_SCANCODE_2),
-  KeySDLKeyPair(Key::Nine,      SDL_SCANCODE_9),
-  KeySDLKeyPair(Key::Three,      SDL_SCANCODE_3),
-};
-"""
 
-EXP = "constexpr static KeySDLKeyPair sKeyPairs\\[] ?= ?\\{[\\S\\s]*?};"
+def convert_inputs(remapped_inputs):
+    key_pairs = "constexpr static KeySDLKeyPair sKeyPairs[] = {\n"
+
+    for ion_code, scancode in inputs:
+        key_pair = f"  KeySDLKeyPair({ion_code},    SDL_SCANCODE_{scancode}),\n"
+        key_pairs = key_pairs + key_pair
+    key_pairs = key_pairs + "};"
+
+    return key_pairs
+
+
+def remap_file(sim_input_file, key_pairs):
+    content = ""
+    remapped_content = ""
+
+    with open(sim_input_file, "r") as f:
+        content = f.read()
+
+    if key_pairs not in content:
+        remapped_content = re.sub(REMAP_EXP, key_pairs, content)
+
+        with open(sim_input_file, "w") as f:
+            f.write(remapped_content)
+
+        info("remapped inputs of " + sim_dir)
+    else:
+        info(sim_dir + " inputs already remapped")
+
 
 sim_dir = sys.argv[1]
-input_file = sim_dir + "/ion/src/simulator/shared/keyboard.cpp"
-content = ""
-remapped_content = ""
+sim_input_file = sim_dir + "/ion/src/simulator/shared/keyboard.cpp"
+app_controls_file = "src/constants.rs"
 
-with open(input_file, "r") as f:
-    content = f.read()
+remapped_inputs = remap_controls(app_controls_file, inputs)
 
-if INPUTS not in content:
-    remapped_content = re.sub(EXP, INPUTS, content)
+key_pairs = convert_inputs(remapped_inputs)
 
-    with open(input_file, "w") as f:
-        f.write(remapped_content)
-
-    info("remapped inputs of " + sim_dir)
-else:
-    info(sim_dir + " inputs already remapped")
+remap_file(sim_input_file, key_pairs)
