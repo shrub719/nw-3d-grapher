3b4c60d395ca8d0ea92c7ed51ed9aa3d9eef45e5
shrub <shrub719@gmail.com>
2025-11-08 15:22:08 +0000

    add another fun implicit function

--- src/generator/implicit.rs
+++ src/generator/implicit.rs
@@ -8,10 +8,26 @@ use crate::{
 };
 use heapless::Vec;
 
+fn benchmark_func(x: f32, y: f32, z: f32) -> f32 {
+    let x2 = x * x;
+    let y2 = y * y;
+    let z2 = z * z;
+
+    let a = 2.92 * (x - 1.0) * x2 * (x + 1.0) + 1.7 * y2;
+    let b = 2.92 * (y - 1.0) * y2 * (y + 1.0) + 1.7 * z2;
+    let c = 2.92 * (z - 1.0) * z2 * (z + 1.0) + 1.7 * x2;
+
+    let term1 = a * a * (y2 - 0.88) * (y2 - 0.88);
+    let term2 = b * b * (z2 - 0.88) * (z2 - 0.88);
+    let term3 = c * c * (x2 - 0.88) * (x2 - 0.88);
+
+    term1 + term2 + term3 - 0.02   
+}
+
 fn placeholder_func(x: f32, y: f32, z: f32) -> f32 {
     x*x*x*x + 2.0*x*x*y*y + 2.0*x*x*z*z + y*y*y*y + 2.0*y*y*z*z + z*z*z*z + 8.0*x*y*z - 10.0*x*x - 10.0*y*y - 10.0*z*z + 20.0
     // 400.0 * (x*x*y*y + y*y*z*z + x*x*z*z) - (1.0-x*x-y*y-z*z)*(1.0-x*x-y*y-z*z)*(1.0-x*x-y*y-z*z)
-}
+    }
 
 fn test_surface(v: Vector3) -> f32 {
     placeholder_func(v.x, v.y, v.z)
