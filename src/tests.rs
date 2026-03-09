9cc55f475f56586e47747dc121cbd2ad4db246fa
shrub <shrub719@gmail.com>
2026-03-09 12:33:11 +0000

    add some more tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,15 +1,55 @@
-fn test_1_1_a() -> bool {
-    print!("1.1.a");
+use crate::*;
 
-    use crate::expr::parser::*;
+fn test_tokenisation(expr: &str, expected: Vec<expr::parser::Token>) -> bool {
+    use expr::parser::*;
 
-    let expr = Expr::new("3", true).unwrap();
-    
-    expr.rpn.into_iter().collect::<Vec<_>>() == vec!(Token::Const(3.0))
+    let expr = Expr::new(expr, true).unwrap();
+
+    expr.rpn.into_iter().collect::<Vec<_>>() == expected
 }
 
-const TESTS: [fn() -> bool; 1] = [
-    test_1_1_a
+fn t11a() -> bool {
+    print!("1.1.a");
+
+    use expr::parser::Token::*;
+    test_tokenisation(
+        "3",
+        vec!(Const(3.0))
+    )
+}
+
+fn t11b() -> bool {
+    print!("1.1.b");
+
+    use expr::parser::Token::*;
+    test_tokenisation(
+        "1 1 +",
+        vec!(Const(1.0), Const(1.0), Add)
+    )
+}
+
+fn t11c() -> bool {
+    print!("1.1.c");
+
+    use expr::parser::Token::*;
+    test_tokenisation(
+        "x",
+        vec!(X)
+    )
+}
+
+fn t11d() -> bool {
+    print!("1.1.d");
+
+    use expr::parser::Token::*;
+    test_tokenisation(
+        "0 sin",
+        vec!(Const(0.0), Sin)
+    )
+}
+
+const TESTS: &[fn() -> bool] = &[
+    t11a, t11b, t11c, t11d
 ];
 
 pub fn run_all() {
